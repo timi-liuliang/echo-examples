a6d7aa75fe534b847a06715cae36e7222cb37d13
LiuLiang <qq79402005@gmail.com>
2018-09-20 21:37:14 +0800

    linear regression

--- neural_network/linear_regression/train.lua
+++ neural_network/linear_regression/train.lua
@@ -8,15 +8,15 @@ function object:start()
 	dataViewer    = self:getNode("data")
 	
 	-- train function y = 2.0 * x
-	neuralNetWork:train({1}, {1})
-	neuralNetWork:train({2}, {4})
-	neuralNetWork:train({3}, {6})
-	neuralNetWork:train({4}, {8})
-	neuralNetWork:train({5}, {10})
-	neuralNetWork:train({6}, {12})
+	neuralNetWork:train({{1}}, {{1}})
+	neuralNetWork:train({{2}}, {{4}})
+	neuralNetWork:train({{3}}, {{6}})
+	neuralNetWork:train({{4}}, {{8}})
+	neuralNetWork:train({{5}}, {{10}})
+	neuralNetWork:train({{6}}, {{12}})
 	
 	-- test
-	local output = neuralNetWork:computeOutput({1})
+	local output = neuralNetWork:computeOutput({{1}})
 	for k,v in pairs(output) do
 		Log:info(v)
 	end
