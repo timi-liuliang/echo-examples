24e0efcc589d7cfd79795922ac8af29b0437b5bc
captain <qq79402005@gmail.com>
2019-12-07 21:17:39 +0800

    Setting GamePad Ui

--- captain/lua/ui/gamepad/left_control.lua
+++ captain/lua/ui/gamepad/left_control.lua
@@ -1,11 +1,25 @@
 local object ={}
 
+object.bg = nil
+
 -- start
 function object:start()
+	self.bg = self:getNode("bg")
 end
 
 -- update
 function object:update()
 end
 
-return setmetatable(object, Object)
+function object:on_clicked_UiEventRegionRect()
+	-- change bg position
+	Log:error("a")
+	local localPos = self.bg:getLocalPosition()
+	Log:error("b")
+	localPos.x = localPos.x + 10
+	Log:error("c")
+	self.bg:setLocalPosition(localPos)
+	Log:error("d")
+end
+
+return setmetatable(object, Node)
