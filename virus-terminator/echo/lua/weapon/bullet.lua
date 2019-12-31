84a53fe7b4d4c1670b0c7c51bcf1e6583442f666
captain <qq79402005@gmail.com>
2019-12-31 20:32:30 +0800

    bullet support life time

--- virus-terminator/echo/lua/weapon/bullet.lua
+++ virus-terminator/echo/lua/weapon/bullet.lua
@@ -1,7 +1,7 @@
 local object ={}
 object.moveDir = vec3(0.0, 0.0, 0.0)
 object.moveSpeed = 2.0
-object.life = 5.0
+object.life = 3.5
 
 -- start
 function object:start()
@@ -9,15 +9,28 @@ end
 
 -- update
 function object:update()
+	-- update position
 	local curPos = self:getLocalPosition()
 	curPos = curPos + self.moveDir * self.moveSpeed
 	
 	self:setLocalPosition(curPos)
+	
+	-- update life
+	self:updateLife()
 end
 
 -- set move direction
 function object:setMoveDir(moveDir)
 	self.moveDir = moveDir:normalize()
+end
+
+-- update life
+function object:updateLife()
+	local elapsedTime = Engine:getFrameTime()
+	self.life = self.life - elapsedTime
+	if self.life <= 0.0 then
+		self:queueFree()
+	end
 end
 
 return setmetatable(object, Object)
