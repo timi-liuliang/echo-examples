efec381b4bdd1e2b1c1303674ddd98c80cf7e11b
captain <qq79402005@gmail.com>
2020-01-05 21:40:46 +0800

    config monsters

--- virus-terminator/echo/lua/monster/monster.lua
+++ virus-terminator/echo/lua/monster/monster.lua
@@ -1,17 +1,30 @@
-local object ={}
+local object = {}
 object.elapsedTime = 0.0
+object.monsters = nil
 
 -- start
 function object:start()
+	-- load level monster configs
+	self:loadLevelMonsters()
 end
 
 -- update
 function object:update()
+	-- spawn monsters
+	self:spawnMonsters()
+end
+
+-- spawn monsters
+function object:spawnMonsters(elapsedTime)
 	self.elapsedTime = self.elapsedTime + Engine:getFrameTime()
-	if self.elapsedTime > 5.0 then
-		self:spawnMonster()
-		
-		self.elapsedTime = 0.0
+	
+	for k, v in pairs(self.monsters) do
+		local monster = self.monsters[k]
+		if self.elapsedTime > monster.time then
+			self:spawnMonster()
+			
+			self.monsters[k] = nil
+		end
 	end
 end
 
@@ -21,6 +34,20 @@ function object:spawnMonster()
 	if newMonster ~= nil then
 		newMonster:setWorldPosition(self:getWorldPosition())
 		newMonster:setParent(self)
+	end
+end
+
+-- load level monster config
+function object:loadLevelMonsters()
+	local xmlContent = IO:loadFileToString("Res://config/monsters_spawn_config.xml")
+	if xmlContent ~= nil then
+		local xmlParser = require("lua.util.xmlSimple").newParser()
+		local spawnMonstersConfig = xmlParser:ParseXmlText(xmlContent)
+		self.monsters = spawnMonstersConfig.monsters:children()
+		for i = 1, #self.monsters do
+			local monster = self.monsters[i]
+			monster.time = tonumber(monster["@time"])
+		end	
 	end
 end
 
--- virus-terminator/echo/lua/monster/monster_ai.lua
+++ virus-terminator/echo/lua/monster/monster_ai.lua
@@ -4,21 +4,31 @@ object.target = nil
 
 -- start
 function object:start()
-	-- set target
-	self.target = self:getNode("/root/hero")
 end
 
 -- update
 function object:update()
-	local targetPos = self.target:getWorldPosition()
-	local curPos = self:getWorldPosition()
-	local dirWithLen = targetPos - curPos
-	if dirWithLen:length() > 3.0 then
-		local nextPos = curPos + dirWithLen:normalize() * self.moveSpeed
-		self:setWorldPosition(nextPos)
-	else
-		self:queueFree()
-	end	
+	-- set target
+	self:initTarget()
+	
+	if self.target~= nil then	
+		local targetPos = self.target:getWorldPosition()
+		local curPos = self:getWorldPosition()
+		local dirWithLen = targetPos - curPos
+		if dirWithLen:length() > 3.0 then
+			local nextPos = curPos + dirWithLen:normalize() * self.moveSpeed
+			self:setWorldPosition(nextPos)
+		else
+			self:queueFree()
+		end
+	end
+end
+
+-- init target
+function object:initTarget()
+	if self.target == nil then
+		self.target = self:getNode("/root/hero")	
+	end
 end
 
 return setmetatable(object, Object)
