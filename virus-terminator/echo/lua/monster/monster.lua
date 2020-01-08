7abd27eda675e1e93353fb077e8ca6d0718bab9d
captain <qq79402005@gmail.com>
2020-01-09 00:04:57 +0800

    Spwan monster support count

--- virus-terminator/echo/lua/monster/monster.lua
+++ virus-terminator/echo/lua/monster/monster.lua
@@ -1,7 +1,10 @@
 local object = {}
 object.elapsedTime = 0.0
 object.hero = nil
+object.waves = nil
+object.currentWave = 0
 object.monsters = nil
+object.isGameOver = false
 
 -- start
 function object:start()
@@ -21,16 +24,23 @@ end
 function object:spawnMonsters(elapsedTime)
 	self.elapsedTime = self.elapsedTime + Engine:getFrameTime()
 	
+	if #self.monsters <=0 and self:getChildNum()<=0 then
+		self:loadNextWaveMonsters()
+	end
+	
 	for k, v in pairs(self.monsters) do
 		local monster = self.monsters[k]
 		if self.elapsedTime > monster.time then
 			-- calc position
-			local range = monster.angle_range
-			local angle = math.rad(monster.angle + math.random(-range, range))
-			local dir   = vec3(math.cos(angle), math.sin(angle), 0.0)
-			local pos   = dir:normalize() * monster.distance + self.hero:getWorldPosition()
+			local count = monster.count
+			for i = 1, count do
+				local range = monster.angle_range
+				local angle = math.rad(monster.angle + math.random(-range, range))
+				local dir   = vec3(math.cos(angle), math.sin(angle), 0.0)
+				local pos   = dir:normalize() * monster.distance + self.hero:getWorldPosition()
 			
-			self:spawnMonster(pos)
+				self:spawnMonster(pos)
+			end
 			
 			self.monsters[k] = nil
 		end
@@ -46,20 +56,46 @@ function object:spawnMonster(pos)
 	end
 end
 
--- load level monster config
-function object:loadLevelMonsters()
-	local xmlContent = IO:loadFileToString("Res://config/monsters_spawn_config.xml")
-	if xmlContent ~= nil then
-		local xmlParser = require("lua.util.xmlSimple").newParser()
-		local spawnMonstersConfig = xmlParser:ParseXmlText(xmlContent)
-		self.monsters = spawnMonstersConfig.monsters:children()
+-- load next wave monsters
+function object:loadNextWaveMonsters()
+	-- return if game is over
+	if self.isGameOver then return end	
+		
+	-- generate new monsters
+	self.currentWave = self.currentWave + 1
+	self.elapsedTime = 0.0
+	
+	Log:info("wave : " .. self.currentWave)
+	
+	if self.currentWave < #self.waves then
+		local wave = self.waves[self.currentWave]
+		
+		self.monsters = wave:children()
 		for i = 1, #self.monsters do
 			local monster = self.monsters[i]
 			monster.time 			= tonumber(monster["@time"])
 			monster.angle 			= tonumber(monster["@angle"])
 			monster.angle_range 	= tonumber(monster["@anglerange"])
 			monster.distance 		= tonumber(monster["@distance"])
-		end	
+			monster.count			= tonumber(monster["@count"])
+		end
+	else
+		-- Game victory
+		self.isGameOver = true
+		
+		Log:info("Game Victory")
+	end
+end
+
+-- load level monster config
+function object:loadLevelMonsters()
+	local xmlContent = IO:loadFileToString("Res://config/monsters_spawn_config.xml")
+	if xmlContent ~= nil then
+		local xmlParser = require("lua.util.xmlSimple").newParser()
+		local spawnMonstersConfig = xmlParser:ParseXmlText(xmlContent)
+		self.waves = spawnMonstersConfig.monsters:children()
+
+		self:loadNextWaveMonsters()
 	end
 end
 
