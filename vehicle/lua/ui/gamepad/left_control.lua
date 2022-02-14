98e39d04c0db254ea88ce446ed551bd71e5c1fbc
Captain <qq79402005@gmail.com>
2022-02-14 23:32:41 +0800

    physx vehicle turn

---
+++ vehicle/lua/ui/gamepad/left_control.lua
@@ -0,0 +1,71 @@
+local object ={}
+
+object.bg = nil
+object.ball =nil
+object.clickPos = nil
+object.car = nil
+object.isMoveing = false
+object.moveDir = nil
+
+-- start
+function object:start()
+	self.bg = self:getNode("bg")
+	self.ball = self:getNode("bg/ball")
+	self.car = self:getNode("/root/car")
+end
+
+-- update
+function object:update()
+	if self.isMoveing then
+		--self.car:move(self.moveDir)
+	end
+end
+
+-- on click region
+function object:onMouseButtonDown_left()
+	-- change bg position
+	local event = self:getMouseEvent()
+	
+	self.clickPos = event:getLocalPosition()
+	self.bg:setLocalPosition(self.clickPos)
+	
+	self.isMoveing = true
+end
+
+-- on mouse moved
+function object:onMouseButtonMove_left()
+	if Input:isMouseButtonDown(0) then
+		-- change ball position
+		local event = self:getMouseEvent()
+	
+		local curPos = event:getLocalPosition()	
+		local offset = curPos - self.clickPos	
+		
+		-- move terminator
+		if offset:length() > 3.0 then
+			self.moveDir = offset:normalize()
+		end
+		
+		-- modify ball position
+		if offset:length() > 70.0 then
+			offset = offset:normalize() * 70.0			
+		end
+		self.ball:setLocalPosition(offset)
+	end
+end
+
+-- on mouse up
+function object:onMouseButtonUp_left()
+	local zeroPosition = vec3(0.0, 0.0, 0.0)
+	self.bg:setLocalPosition(zeroPosition)
+	self.ball:setLocalPosition(zeroPosition)
+
+	self.isMoveing = false
+	self.moveDir = vec3(0.0, 0.0, 0.0)
+end
+
+function object:onMouseButtonLeave()
+	self:onMouseButtonUp_left()
+end
+
+return setmetatable(object, Node)
